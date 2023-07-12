6dbfcbd0056ad9594dadc32c0d771154b22f469b
Jakub Šlof <jakub.sloff@gmail.com>
2023-07-12 19:05:18 +0200

    vytvori object_detection soubor

---
+++ object_detection_espcam.py
@@ -0,0 +1,34 @@
+#program na nalezeni medveda pomoci kamery 
+from ultralytics import YOLO
+import numpy as np
+import cv2 
+import cvzone
+import urllib.request
+
+url = 'http://192.168.137.92/cam-lo.jpg'#url adresa esp cam 
+
+model = YOLO('yolov8n.pt')#zvoleni jaky model se pouzije 
+
+cap = cv2.VideoCapture(url)#nacte video z kamery do promene 
+model = YOLO('yolov8n.pt')
+while True:
+    # Read a frame from the video stream
+    img_resp=urllib.request.urlopen(url)
+    imgnp=np.array(bytearray(img_resp.read()),dtype=np.uint8)
+    #ret, frame = cap.read()
+    im = cv2.imdecode(imgnp,-1)
+    results = model(im, stream=True)
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            x1,y1,x2,y2 = box.xyxy [0]
+            x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
+            print(x1,y1,x2,y2)
+    cv2.imshow('footage',im)
+    key=cv2.waitKey(5)
+    if key==ord('q'):
+        break
+    
+
+cap.release()
+cv2.destroyAllWindows()
