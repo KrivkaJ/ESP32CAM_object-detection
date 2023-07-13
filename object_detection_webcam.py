a6cd5907f24104c44c9b3bea909a3125fea8ce03
Jakub Šlof <jakub.sloff@gmail.com>
2023-07-13 12:36:23 +0200

    prida vypisovani jistoty modelu

--- object_detection_webcam.py
+++ object_detection_webcam.py
@@ -5,6 +5,8 @@ import cv2
 
 model = YOLO('yolov8n.pt')#zvoleni jaky model se pouzije 
 cap = cv2.VideoCapture(0)#nacte video z kamery do promene
+cap.set(3,720)
+cap.set(4,480)
 while True:
      sucess, img = cap.read()
      results = model(img, stream = True)
@@ -14,6 +16,10 @@ while True:
             x1,y1,x2,y2 = box.xyxy [0]
             x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)#prevedeni hodnot na int pro lepsi praci s nima 
             print(x1,y1,x2,y2)#vypisuje velikost objektu a jeho polohu v px 
-            cv2.rectangle(im,(x1,y1),(x2,y2),(255,0,255),3)#nakresli box okolo detekovane veci 
-            cv2.imshow('footage',img)
-            key=cv2.waitKey(1)
+            cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3)#nakresli box okolo detekovane veci 
+            conf = box.conf[0]#jistota modelu 
+            conf = float(conf*100)
+            rounded_conf = int(conf)#zaokrouhli jistotu modelu na dve desetina mista 
+            print('confidence:',rounded_conf)
+     cv2.imshow('footage',img)
+     cv2.waitKey(1000)#delay takze to vyhodnocuje jen jeden frame za sekundu pro odlehceni 
