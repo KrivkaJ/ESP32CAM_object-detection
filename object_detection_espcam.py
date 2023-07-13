bd51c98071615a4e8913d06fde2009344318efbe
Jakub Šlof <jakub.sloff@gmail.com>
2023-07-13 10:23:00 +0200

    prida obdelniky kolem detekovanych objektu

--- object_detection_espcam.py
+++ object_detection_espcam.py
@@ -5,7 +5,7 @@ import cv2
 import cvzone
 import urllib.request
 
-url = 'http://192.168.137.92/cam-lo.jpg'#url adresa esp cam 
+url = 'http://192.168.137.92/cam-hi.jpg'#url adresa esp cam 
 
 model = YOLO('yolov8n.pt')#zvoleni jaky model se pouzije 
 
@@ -22,10 +22,11 @@ while True:
         boxes = r.boxes
         for box in boxes:
             x1,y1,x2,y2 = box.xyxy [0]
-            x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
-            print(x1,y1,x2,y2)
+            x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)#prevedeni hodnot na int pro lepsi praci s nima 
+            print(x1,y1,x2,y2)#vypisuje velikost objektu a jeho polohu v px 
+            cv2.rectangle(im,(x1,y1),(x2,y2),(255,0,255),3)#nakresli box okolo detekovane veci 
     cv2.imshow('footage',im)
-    key=cv2.waitKey(5)
+    key=cv2.waitKey(1)
     if key==ord('q'):
         break
     
